418f85997e4c5fde08f4edfe4e9ecb6b39690109
Bercea Cristian <cristiberc@gmail.com>
2026-01-09 20:41:35 +0200

    modified player entity+repo and added regions table

--- backend/domain/entities/Player.py
+++ backend/domain/entities/Player.py
@@ -8,8 +8,7 @@ class Player:
             "avg_play_time": 0.0,
             "quizzes_solved": 0,
             "quizzes_played": 0,
-            "regions_unlocked": ["Transilvania"],
-            "cosmetics_unlocked": [self.__cosmetic],
+            "regions_state": {},
             "cosmetics_purchased": [self.__cosmetic],
             "completion_percentage": 0.0
         }
@@ -28,15 +27,13 @@ class Player:
     def get_quizzes_played(self): return self.__statistics["quizzes_played"]
     def get_regions_unlocked(self): return self.__statistics["regions_unlocked"]
     def get_completion_percentage(self): return self.__statistics["completion_percentage"]
-    def get_cosmetics_unlocked(self): return self.__statistics["cosmetics_unlocked"]
     def get_cosmetics_purchased(self): return self.__statistics["cosmetics_purchased"]
 
     def set_avg_play_time(self, avg_play_time: float): self.__statistics["avg_play_time"] = avg_play_time
     def set_quizzes_solved(self, quizzes_solved: int): self.__statistics["quizzes_solved"] = quizzes_solved
     def set_quizzes_played(self, quizzes_played: int): self.__statistics["quizzes_played"] = quizzes_played
-    def set_regions_unlocked(self, regions_unlocked: list[str]): self.__statistics["regions_unlocked"] = regions_unlocked
+    def set_regions_state(self, regions_state: dict[str,int]): self.__statistics["regions_state"] = regions_state
     def set_completion_percentage(self, completion_percentage: float): self.__statistics["completion_percentage"] = completion_percentage
-    def set_cosmetics_unlocked(self, cosmetics_unlocked: list[str]): self.__statistics["cosmetics_unlocked"] = cosmetics_unlocked
     def set_cosmetics_purchased(self, cosmetics_purchased: list[str]): self.__statistics["cosmetics_purchased"] = cosmetics_purchased
 
     def __str__(self): return f"Player {self.__name} (id: {self.__id})\nCredits: {self.__credits} \nEquipped cosmetic {self.__cosmetic} \nStatistics: {self.__statistics}"
--- backend/repository/PlayerRepository.py
+++ backend/repository/PlayerRepository.py
@@ -1,72 +1,70 @@
 import sqlite3
 from typing import Optional
-
 from backend.domain.entities.Player import Player
-
 
 class PlayerRepository:
     def __init__(self, conn: sqlite3.Connection):
         self.conn = conn
         self.cursor = self.conn.cursor()
         self.TABLE = "player"
+        self.REGIONS_TABLE = "regions"
 
     def save(self, player) -> None:
         stats = player.get_statistics()
+        try:
+            self.cursor.execute(f"SELECT id FROM {self.TABLE} WHERE id = ?", (player.get_id(),))
+            exists = self.cursor.fetchone()
+            cosmetics_purchased = '|'.join(stats['cosmetics_purchased'])
+            if exists:
+                sql = f"""
+                    UPDATE {self.TABLE} 
+                    SET name = ?, credits = ?, avg_play_time = ?, quizzes_solved = ?, quizzes_played = ?,
+                        cosmetics_purchased = ?, completion_percentage = ?, equipped_cosmetic = ? WHERE id = ?
+                    """
+                self.cursor.execute(sql, (
+                    player.get_name(), player.get_credits(),
+                    stats['avg_play_time'], stats['quizzes_solved'], stats['quizzes_played'],
+                    cosmetics_purchased, stats['completion_percentage'], player.get_cosmetic(), player.get_id()
+                ))
 
-        self.cursor.execute(f"SELECT id FROM {self.TABLE} WHERE id = ?", (player.get_id(),))
-        exists = self.cursor.fetchone()
+                for region, state in stats['regions_state'].items():
+                    self.cursor.execute(f"UPDATE {self.REGIONS_TABLE} SET state = ? WHERE region = ?", (state, region))
+            else:
+                sql = f"""
+                    INSERT INTO {self.TABLE} (name, credits, avg_play_time, quizzes_solved, quizzes_played, cosmetics_purchased, completion_percentage) 
+                    VALUES (?, ?, ?, ?, ?, ?, ?)
+                    """
+                self.cursor.execute(sql, (
+                    player.get_name(), player.get_credits(), stats['avg_play_time'], stats['quizzes_solved'],
+                    stats['quizzes_played'], cosmetics_purchased, stats['completion_percentage']
+                ))
 
-        regions_unlocked = '|'.join(stats['regions_unlocked'])
-        cosmetics_unlocked = '|'.join(stats['cosmetics_unlocked'])
-        cosmetics_purchased = '|'.join(stats['cosmetics_purchased'])
+            self.conn.commit()
+        except sqlite3.Error as _: self.conn.rollback()
 
-        if exists:
-            sql = f"""
-                UPDATE {self.TABLE} 
-                SET name = ?, credits = ?, avg_play_time = ?, quizzes_solved = ?, 
-                    quizzes_played = ?, regions_unlocked = ?, cosmetics_unlocked = ?, 
-                    cosmetics_purchased = ?, completion_percentage = ?
-                WHERE id = ?
-            """
-            self.cursor.execute(sql, (
-                player.get_name(), player.get_credits(),
-                stats['avg_play_time'], stats['quizzes_solved'], stats['quizzes_played'],
-                regions_unlocked, cosmetics_unlocked, cosmetics_purchased,
-                stats['completion_percentage'], player.get_id()
-            ))
-        else:
-            sql = f"""
-                INSERT INTO {self.TABLE} (id, name, credits, avg_play_time, quizzes_solved, quizzes_played, regions_unlocked, cosmetics_unlocked, cosmetics_purchased, completion_percentage) 
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-            """
-            self.cursor.execute(sql, (
-                player.get_id(), player.get_name(), player.get_credits(),
-                stats['avg_play_time'], stats['quizzes_solved'], stats['quizzes_played'],
-                regions_unlocked, cosmetics_unlocked, cosmetics_purchased,
-                stats['completion_percentage']
-            ))
-
-        self.conn.commit()
-
-    def get(self) -> Optional[object]:
-        self.cursor.execute(f"SELECT * FROM {self.TABLE} LIMIT 1")
-        row = self.cursor.fetchone()
+    def get(self) -> Optional[Player]:
+        self.conn.row_factory = sqlite3.Row  # Access columns by name
+        cursor = self.conn.cursor()
+        cursor.execute(f"SELECT * FROM {self.TABLE} LIMIT 1")
+        row = cursor.fetchone()
 
         if row:
-            player = Player(row[0], row[1])
-            player.set_credits(row[2])
-            player.set_cosmetic(row[11])
-            player.set_avg_play_time(row[3])
-            player.set_quizzes_solved(row[4])
-            player.set_quizzes_played(row[5])
-            player.set_regions_unlocked(row[6].split('|') if row[6] else [])
-            player.set_cosmetics_unlocked(row[7].split('|') if row[7] else [])
-            player.set_cosmetics_purchased(row[8].split('|') if row[8] else [])
-            player.set_completion_percentage(row[9])
+            player = Player(row['id'], row['name'])
+            player.set_credits(row['credits'])
+            player.set_avg_play_time(row['avg_play_time'])
+            player.set_quizzes_solved(row['quizzes_solved'])
+            player.set_quizzes_played(row['quizzes_played'])
+            player.set_cosmetics_purchased(row['cosmetics_purchased'].split('|') if row['cosmetics_purchased'] else [])
+            player.set_completion_percentage(row['completion_percentage'])
+            player.set_cosmetic(row['equipped_cosmetic'])
 
+            cursor.execute(f"SELECT region, state FROM {self.REGIONS_TABLE}")
+            player.set_regions_state({r['region']: r['state'] for r in cursor.fetchall()})
             return player
         return None
 
     def delete(self, player_id: int) -> None:
         self.cursor.execute(f"DELETE FROM {self.TABLE} WHERE id = ?", (player_id,))
+        for region_id in range(1, 6):
+            self.cursor.execute(f"UPDATE {self.REGIONS_TABLE} SET state = 1 WHERE id = {region_id}")
         self.conn.commit()
